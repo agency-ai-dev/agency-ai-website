bda318ad20badecd5a61e6e1108d55f867120d59
johnkirtley <kirtleyj16@gmail.com>
2025-10-01 10:34:00 -0700

    waitlist prep

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,9 +4,21 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-ssr/
  */
 
+const React = require("react")
+
 /**
  * @type {import('gatsby').GatsbySSR['onRenderBody']}
  */
-exports.onRenderBody = ({ setHtmlAttributes }) => {
+exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
   setHtmlAttributes({ lang: `en` })
+  setHeadComponents([
+    React.createElement("script", {
+      key: "analytics",
+      defer: true,
+      "data-website-id": "68dd6400978afa4e3dbb5ef5",
+      "data-domain": "agencyai.app",
+      "data-allow-localhost": "true",
+      src: "https://datafa.st/js/script.js",
+    }),
+  ])
 }
